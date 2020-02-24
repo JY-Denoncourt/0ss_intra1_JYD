04d34a93120050cca047143132640624cc501019
Jean-Yves Denoncourt <masterlopette@hotmail.com>
2020-02-24 13:32:15 -0500

    Class fait

--- wpf_intra/intra_models/Customer.cs
+++ wpf_intra/intra_models/Customer.cs
@@ -1,8 +1,117 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace intra_models
 {
-    public class Customer
+    public class Customer : INotifyPropertyChanged
     {
+        private string name;
+        private string lastName;
+        private string address;
+        private string city;
+        private string province;
+        private string postalCode;
+        private string contactInfo;
+        private string picturePath;
+
+
+        /* --------------------------------Methode de class ------------------------------------------- */
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged();
+            }
+        }
+       
+        public string Address
+        {
+            get => address;
+            set
+            {
+                address = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string City
+        {
+            get => city;
+            set
+            {
+                city = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Province
+        {
+            get => province;
+            set
+            {
+                province = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PostalCode
+        {
+            get => postalCode;
+            set
+            {
+                postalCode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ContactInfo
+        {
+            get => contactInfo;
+            set
+            {
+                contactInfo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PicturePath
+        {
+            get => picturePath;
+            set
+            {
+                picturePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+        public string Info => $"{LastName} , {Name}";
+
+
+       
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
+}
 }
